db84874ea49f68f30e8db110cbfcd8e650b588d9
duykhanp7 <duykhangp7@gmail.com>
2022-12-20 22:26:25 +0700

    Create splash screen

---
+++ lib/res/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/animation.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color primary = Color(0xFF00CEA6);
+  static const Color white = Color(0xFFFFFFFF);
+}
