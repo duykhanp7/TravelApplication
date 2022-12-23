bc6644fec359b3ac2f32eaeed2444ec404f92b77
duykhanp7 <duykhangp7@gmail.com>
2022-12-23 22:27:05 +0700

    Create_onboarding_screen

--- lib/res/colors.dart
+++ lib/res/colors.dart
@@ -5,4 +5,10 @@ class AppColors {
 
   static const Color primary = Color(0xFF00CEA6);
   static const Color white = Color(0xFFFFFFFF);
+  static const Color textOnboardingBrown = Color(0xFF555555);
+  static const Color textOnboardingBlack = Color(0xFF121212);
+  static const Color textSkipColor = Color(0xFF777777);
+  static const Color indicatorOnboardingColor = Color(0xFFDBDBDB);
+  static const Color transparent = Color(0x00000000);
+  static const Color buttonRipple = Color(0xFF03DFB3);
 }
---
+++ lib/res/images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  const AppImages._();
+
+  static const String parentPathImages = 'assets/images';
+  static const String childPathImages = '/onboarding';
+
+  static const String card1 = '$parentPathImages$childPathImages/card1.png';
+  static const String card2 = '$parentPathImages$childPathImages/card2.png';
+}
