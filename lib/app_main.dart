f1500da4fd243fbe87ac878025c3466321badbbf
duykhanp7 <duykhangp7@gmail.com>
2022-12-25 14:07:10 +0700

    Create forgot password screen

--- lib/app_main.dart
+++ lib/app_main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:travel_booking_tour/features/signin/signin_screen.dart';
-import 'package:travel_booking_tour/features/signup/sign_up_screen.dart';
+import 'package:travel_booking_tour/features/forgot_password/forgot_password_screen.dart';
 import 'package:travel_booking_tour/res/colors.dart';
 
 import 'l10n/generated/l10n.dart';
@@ -14,7 +13,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: "Fellow 4U",
       theme: ThemeData(backgroundColor: AppColors.white, fontFamily: 'Roboto'),
-      home: const SignInScreen(),
+      home: const ForgotPasswordScreen(),
       debugShowCheckedModeBanner: false,
       localizationsDelegates: const <LocalizationsDelegate<Object>>[
         SLocalization.delegate,
