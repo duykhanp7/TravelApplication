cce5eec79edaf05df323ba8f0a9fb0b10ed81f16
Phạm Duy Khang <62589349+duykhanp7@users.noreply.github.com>
2023-01-02 15:21:09 +0700

    Revert "Create splash screen"
    This reverts commit 40e9234b02d9da32a508963a88873534c2a3a28c.

--- lib/res/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/animation.dart';
-
-class AppColors {
-  const AppColors._();
-
-  static const Color primary = Color(0xFF00CEA6);
-  static const Color white = Color(0xFFFFFFFF);
-}
